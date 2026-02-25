c7e3be5cbe42dfb63d9f60d5770757b192265a34
Andrei Davydov <agdavydov81@gmail.com>
2026-02-25 18:59:08 +0300

    Add navigation drawler

--- app/src/main/java/io/github/agdavydov81/MainActivity.java
+++ app/src/main/java/io/github/agdavydov81/MainActivity.java
@@ -1,38 +1,87 @@
 package io.github.agdavydov81;
 
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.card.MaterialCardView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         MaterialCardView classificationCard = findViewById(R.id.classification_card);
         MaterialCardView regressionCard = findViewById(R.id.regression_card);
         MaterialCardView clusteringCard = findViewById(R.id.clustering_card);
 
-        classificationCard.setOnClickListener(v -> Toast.makeText(MainActivity.this, "Classification Clicked", Toast.LENGTH_SHORT).show());
+        classificationCard.setOnClickListener(v -> OnClassificationSelected());
+        regressionCard.setOnClickListener(v -> OnRegressionSelected());
+        clusteringCard.setOnClickListener(v -> OnClusterizationSelected());
 
-        regressionCard.setOnClickListener(v -> Toast.makeText(MainActivity.this, "Regression Clicked", Toast.LENGTH_SHORT).show());
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        clusteringCard.setOnClickListener(v -> Toast.makeText(MainActivity.this, "Clusterization Clicked", Toast.LENGTH_SHORT).show());
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    if (isEnabled()) {
+                        setEnabled(false);
+                        onBackPressed();
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.nav_classification) {
+            OnClassificationSelected();
+        } else if (itemId == R.id.nav_regression) {
+            OnRegressionSelected();
+        } else if (itemId == R.id.nav_clusterization) {
+            OnClusterizationSelected();
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void OnClassificationSelected() {
+        Toast.makeText(this, "Classification selected", Toast.LENGTH_SHORT).show();
+    }
+
+    public void OnRegressionSelected() {
+        Toast.makeText(this, "Regression selected", Toast.LENGTH_SHORT).show();
+    }
+
+    public void OnClusterizationSelected() {
+        Toast.makeText(this, "Clusterization selected", Toast.LENGTH_SHORT).show();
     }
 }
